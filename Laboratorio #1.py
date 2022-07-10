780b2e74bb356ed636d7d4f0d18fe7146a8ea499
Edward Guerra 52 <106791362+EdwardGuerra25@users.noreply.github.com>
2022-07-09 23:20:13 -0500

    Add files via upload

---
+++ Laboratorio #1.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Laboratorio 1°
+# ## Edward Guerra 8-1005-1529 | Programacion de Ingenieros 2022-2
+
+# In[10]:
+
+
+#Problema 1
+
+
+# In[4]:
+
+
+Inversión=100 
+Años=7 
+InteresAnual=10 
+interesCompuesto= 100*(1+0.1)**7
+
+print(interesCompuesto)
+
+
+# In[11]:
+
+
+#Problema 2
+
+
+# ahorros=100 
+# 
+# print(ahorros)
+
+# In[ ]:
+
+
+#Problema 3
+
+
+# In[6]:
+
+
+ahorros= 100 
+mult_anual=float(1.1)
+resultado= ahorros*(mult_anual)**7 
+
+print(resultado)
+
+
+# In[12]:
+
+
+#Problema 4
+
+
+# In[7]:
+
+
+desc=str("cadena de texto")
+print(desc)
+profic=bool(True)
+
+print(bool(profic))
+
+
+# In[27]:
+
+
+#Problema 5
+
+
+# In[8]:
+
+
+ahorros = 100
+mult = float(1.1)
+desc = 'interes compuesto'
+
+año1= ahorros*(mult) 
+print(año1)
+print(type(año1))
+
+dobledesc= desc + desc 
+print(dobledesc)
+
+
+# In[13]:
+
+
+#Problema 6
+
+
+# In[20]:
+
+
+ahorros = 100
+resultado = 100*1.1**7
+print("Empezamos con")
+print(100)
+print("y ahora tenemos") 
+print(float (194.87171000000012))
+print("Que bien!")
+
+
+# In[26]:
+
+
+pi_string= "3.1415926" 
+print(type(pi_string))
+
+pi_float= float(pi_string)
+print(pi_float)
+print(type(pi_float))
+
